1b6e2f7b55cdffc4c0a04b69438c9e8653f3d3ff
Tasos L <mail@mail.com>
2020-06-28 17:47:58 +0300

    initial commit

---
+++ src/main/java/com/emergon/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.emergon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class HomeController {
+    
+    @RequestMapping
+    public String welcomePage(){
+        return "welcome";
+    }
+    
+}
